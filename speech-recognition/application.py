ebf0704c172675fe78e13190bd5d5672e6599b74
Miruna Lupascu <mirunastefania2001@gmail.com>
2023-03-19 19:04:44 +0200

    Added stop button in GUI

--- speech-recognition/application.py
+++ speech-recognition/application.py
@@ -5,16 +5,30 @@ from main import Listener
 from tkinter import *
 from tkinter import ttk
 
+class UiManager:
+    def __init__(self, root, listener):
+        self.root = root
+        self.listener = listener
+
+    def destroy(self):
+        self.root.destroy()
+        self.listener.destroy()
+
 def start_gui():
     root = Tk()
     frm = ttk.Frame(root, padding=10)
     frm.grid()
 
     listener = Listener()
+    uiManager = UiManager(root, listener)
 
     ttk.Label(frm, text="Start recording").grid(column=0, row=0)
-    ttk.Button(frm, text="Start", command=listener.run_listener).grid(column=1, row=0)
+    ttk.Button(frm, text="Start", command=listener.run_threaded_listener).grid(column=1, row=0)
 
+    ttk.Label(frm, text="Stop recording").grid(column=0, row=1)
+    ttk.Button(frm, text="Stop", command=listener.stop_listener).grid(column=1, row=1)
+
+    root.protocol('WM_DELETE_WINDOW', uiManager.destroy)
     root.mainloop()
 
 if __name__ == '__main__':
@@ -24,5 +38,7 @@ if __name__ == '__main__':
     server_thread.start()
     gui_thread.start()
  
-    server_thread.join()
     gui_thread.join()
+    server_thread.join()
+
+    quit()
--- speech-recognition/main.py
+++ speech-recognition/main.py
@@ -1,11 +1,13 @@
 import azure.cognitiveservices.speech as speechsdk
 import socketio
 import os
+import threading
+
+AZURE_SUBSCRIPTION_KEY = os.environ.get('AZURE_API')
+AZURE_REGION = os.environ.get('AZURE_REGION') or 'westeurope'
+WS_SERVER_ADDRESS = os.environ.get('WS_SERVER_ADDRESS') or 'http://localhost:3001'
 
 sio = socketio.Client()
-subscription = os.environ.get('AZURE_API')
-speech_config = speechsdk.SpeechConfig(subscription=subscription, region="westeurope")
-speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
 
 @sio.event
 def connect():
@@ -19,7 +21,14 @@ def disconnect():
 class Listener:
     def __init__(self):
         self.sio = sio
-        self.sio.connect('http://localhost:3001')
+        self.sio.connect(WS_SERVER_ADDRESS)
+
+        speech_config = speechsdk.SpeechConfig(
+            subscription=AZURE_SUBSCRIPTION_KEY, 
+            region=AZURE_REGION
+        )
+        self.speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
+        self.runner_thread = None
 
     def continue_listening(self, data):
         return not 'finish' in list(map(lambda sentence: sentence.lower(), data)) and self.should_continue
@@ -28,23 +37,31 @@ class Listener:
     def speech_to_text(self):
         # read the audio data from the default microphone
         print("Start recording...")
-        result = speech_recognizer.recognize_once_async().get()
+        result = self.speech_recognizer.recognize_once_async().get()
 
-        if result == '':
-            return []
-        return [result.text[:-1].lower()]
+        return [result.text.strip('?!.,').lower()]
 
     def run_listener(self):
         alternatives = []
+
         self.should_continue = True
 
         while (self.continue_listening(alternatives)):
-
             alternatives = self.speech_to_text()
-            if len(alternatives) > 0:
-                print(alternatives)
+            if len(alternatives) > 0 and self.should_continue == True:
                 self.sio.emit('onCommand', {'alternatives': alternatives})
-        # self.sio.wait()
+
+        print('Stop listening...')
 
     def stop_listener(self):
         self.should_continue = False
+
+    def run_threaded_listener(self):
+        if self.runner_thread and self.runner_thread.is_alive():
+            return
+        
+        self.runner_thread = threading.Thread(target=self.run_listener).start()
+
+    def destroy(self):
+        self.sio.disconnect()
+        self.sio.wait()
