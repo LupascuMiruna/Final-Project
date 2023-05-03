abfa4cd99744b1b915fdc7cf5f69b89c0c5920f8
Miruna Lupascu <mirunastefania2001@gmail.com>
2023-05-03 13:54:16 +0300

    modified gui interface

--- speech-recognition/application.py
+++ speech-recognition/application.py
@@ -1,9 +1,11 @@
 import threading
+import customtkinter
+import os
 
 from ws_server import start_server
 from main import Listener
 from tkinter import *
-from tkinter import ttk
+from PIL import Image
 
 class UiManager:
     def __init__(self, root, listener):
@@ -15,21 +17,46 @@ class UiManager:
         self.listener.destroy()
 
 def start_gui():
-    root = Tk()
-    frm = ttk.Frame(root, padding=10)
-    frm.grid()
-
     listener = Listener()
-    uiManager = UiManager(root, listener)
 
-    ttk.Label(frm, text="Start recording").grid(column=0, row=0)
-    ttk.Button(frm, text="Start", command=listener.run_threaded_listener).grid(column=1, row=0)
+    file_path = os.path.dirname(os.path.realpath(__file__))
+    image_start = customtkinter.CTkImage(Image.open(file_path + "/start.png"), size = (35, 35))
+    image_stop = customtkinter.CTkImage(Image.open(file_path + "/stop.png"), size = (35, 35))
 
-    ttk.Label(frm, text="Stop recording").grid(column=0, row=1)
-    ttk.Button(frm, text="Stop", command=listener.stop_listener).grid(column=1, row=1)
+    app = customtkinter.CTk()
+    app.geometry("400x300")
+    uiManager = UiManager(app, listener)
 
-    root.protocol('WM_DELETE_WINDOW', uiManager.destroy)
-    root.mainloop()
+
+    frame = customtkinter.CTkFrame(master=app, corner_radius=20)
+    frame.pack(pady=20, padx=20, fill="both", expand=True)
+
+    button_start = customtkinter.CTkButton(master=frame,
+                                        image = image_start,
+                                        text = "Start record",
+                                        corner_radius = 8,
+                                        width = 200,
+                                        height = 30,
+                                        font = ("Arial",20),
+                                        text_color="black",
+                                        border_spacing = 10,
+                                        command=listener.run_threaded_listener)
+    button_start.pack(padx=20, pady=20)
+
+    button_stop = customtkinter.CTkButton(master = frame,
+                                        image = image_stop,
+                                        text = "Stop record",
+                                        corner_radius = 8,
+                                        width = 200,
+                                        height = 30,
+                                        font = ("Arial",20),
+                                        text_color="black",
+                                        border_spacing = 10,
+                                        command=listener.stop_listener)
+    button_stop.pack(padx=20, pady=20)
+
+    app.protocol('WM_DELETE_WINDOW', uiManager.destroy)
+    app.mainloop()
 
 if __name__ == '__main__':
     server_thread = threading.Thread(target=start_server)
